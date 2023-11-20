f96896f284efa49377a7659421657fec4520ba08
monkeymr014 <monkeymr014@gmail.com>
2023-11-20 09:07:06 +0100

    add option js04

--- hw/js04/script.js
+++ hw/js04/script.js
@@ -6,6 +6,8 @@ form.innerHTML = `
    <select class="form-select mb-1">
       <option value="+">+</option>
       <option value="-">-</option>
+      <option value="*">*</option>
+      <option value="/">/</option>
    </select>
    <input type="number" id="secNum" placeholder="Druga liczba" class="form-control mb-1" />
    <button type="submit" class="btn btn-primary mb-3" >Oblicz</button>
